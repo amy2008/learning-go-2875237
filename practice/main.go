99d46dc9a445b378a84cb4a42a9ea4c4055bf3a3
meli <meli@redhat.com>
2024-03-29 16:19:09 +0800

    02_06b

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
+	i1, i2, i3 := 3, 4, 5
+	intSum := i1 + i2 + i3
+	fmt.Println("Integre Sum: ", intSum)
 
-	fmt.Println("Math")
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum: ", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("Float Sum: ", floatSum)
+
+	circleRadius := 15.5
+	circleference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circleference: %.4f\n", circleference) //keep the 4 demical numbers
 
 }
