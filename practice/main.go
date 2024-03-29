a5f8f1571e7a9b2e86ce7ccff19fa8048db88df7
meli <meli@redhat.com>
2024-03-29 15:58:23 +0800

    2_4

--- practice/main.go
+++ practice/main.go
@@ -4,13 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter text: ")
+
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
+
+	fmt.Print("Enter a number: ")
+	numInput, _ := reader.ReadString('\n')
+	fmt.Println("You entered:", numInput)
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(numInput), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of number:", aFloat)
+	}
 
 }
