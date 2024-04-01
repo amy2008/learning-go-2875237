3e9f89d08aefd36dfd9ffda64396eacb16baefe6
meli <meli@redhat.com>
2024-04-01 12:52:44 +0800

    for loop

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,33 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+	for value < 10 {
+		fmt.Println(value)
+		value++
+	}
+
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("sum: ", sum)
+		if sum > 200 {
+			goto theEnd
+		}
+	}
+theEnd:
+	fmt.Println("theEnd")
 }
