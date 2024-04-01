d8e4badbaa42395c295e482ca6f74b45822ab740
meli <meli@redhat.com>
2024-04-01 12:36:23 +0800

    switch

--- practice/main.go
+++ practice/main.go
@@ -7,7 +7,21 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	//rand.Seed(time.Now().Unix())
+	rand.NewSource(time.Now().Unix())
+	//dow := rand.Intn(7) + 1
+	//fmt.Println("Day", dow)
+
+	var result string
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		result = "It's Sunday!"
+		fallthrough
+	case 2:
+		result = "It's Monday!"
+	default:
+		result = "It's some other day!"
+	}
+	fmt.Println(result)
+
 }
