e729dcbe262209051395b7d0babb58cb2be65c3a
meli <meli@redhat.com>
2024-04-01 10:54:06 +0800

    slice map make

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Maps")
+	provinces := make(map[string]string)
+	fmt.Println(provinces)
+
+	provinces["HB"] = "HeBei"
+	provinces["BJ"] = "BeiJing"
+	provinces["TJ"] = "TianJin"
+	fmt.Println(provinces)
+
+	tianjin := provinces["TJ"]
+	fmt.Println(tianjin)
+
+	delete(provinces, "HB")
+	provinces["SD"] = "ShanDong"
+	fmt.Println(provinces)
+
+	for k, v := range provinces {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+
+	keys := make([]string, len(provinces))
+	i := 0
+	for k := range provinces {
+		keys[i] = k
+		i++
+	}
+	fmt.Println(keys)
+	sort.Strings(keys)
+	fmt.Println(keys)
+
+	for i := range keys {
+		fmt.Println(provinces[keys[i]])
+	}
+
 }
