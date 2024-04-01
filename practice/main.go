686139449fb0f2d08216a03fb14aae782d7f4fdf
meli <meli@redhat.com>
2024-04-01 14:34:37 +0800

    Define and call functions. define a multi Sum function with the same type

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	sum := addValues(4, 9)
+	fmt.Println("The sum is ", sum)
+
+	multiSum, count := multiValues(4, 9, 2, 5)
+	fmt.Println("The multiSum is ", multiSum)
+	fmt.Println("The count is ", count)
+}
+
+func addValues(value1, value2 int) int {
+	return value1 + value2
+}
+
+func multiValues(values ...int) (int, int) {
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}
+	return sum, len(values)
 }
