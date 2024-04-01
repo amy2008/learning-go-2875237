821165d1cfef6f838269e66ff389337ced18150d
meli <meli@redhat.com>
2024-04-01 10:01:46 +0800

    pointer

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	p := &anInt
+	fmt.Println("Value of pointer: ", *p)
+	fmt.Println("Address of pointer: ", p)
+
+	value1 := 42.12
+	pointer1 := &value1
+	fmt.Println("Value 1: ", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Value 2: ", *pointer1)
 }
